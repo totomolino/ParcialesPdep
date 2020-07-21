237c377ec4f83738b1444c84a068d87d6f6cc53d
totomolino <totomolino@hotmail.com>
2020-07-21 10:42:03 -0300

    termine vocaloid

--- Logico/Vocaloid/vocaloid.pl
+++ Logico/Vocaloid/vocaloid.pl
@@ -58,6 +58,8 @@ duraMenosDe4(Cancion) :-
 
 %  b)
 
+% concierto(Nombre, Lugar, Fama, Tipo).
+
 concierto(mikuExpo, estadosUnidos, 2000, gigante(2, 6)).
 concierto(magicalMirai, japon, 3000, gigante(3, 10)).
 concierto(vocalektVisions, estadosUnidos, 1000, mediano(9)).
@@ -68,22 +70,25 @@ concierto(mikuFest, argentina, 100 , pequenio(4)).
 puedeParticipar(hatsuneMiku, Concierto) :-
     concierto(Concierto, _, _,_).
 
-puedeParticipar(Vocaloid, Concierto) :-
-    concierto(Concierto, _, _, gigante(MinimoCanciones, TiempoMinimo)),
-    vocaloid(Vocaloid, _),
+puedeParticipar(Cantante, Concierto):-
+	vocaloid(Cantante, _),
+	Cantante \= hatsuneMiku,
+	concierto(Concierto, _, _, Requisitos),
+	cumpleRequisitos(Cantante, Requisitos).
+
+
+
+cumpleRequisitos(Vocaloid, gigante(MinimoCanciones, TiempoMinimo)) :-
     cuantasCanciones(Vocaloid, CuantasCanciones),
     tiempoTotal(Vocaloid, Tiempo),
     CuantasCanciones > MinimoCanciones,
     Tiempo >= TiempoMinimo.
 
-puedeParticipar(Vocaloid, Concierto) :-
-    concierto(Concierto, _, _, mediano(TiempoMaximo)),
-    vocaloid(Vocaloid,_),
+cumpleRequisitos(Vocaloid, mediano(TiempoMaximo)) :-
     tiempoTotal(Vocaloid, Tiempo),
     Tiempo =< TiempoMaximo.
 
-puedeParticipar(Vocaloid, Concierto) :-
-    concierto(Concierto, _ , _, pequenio(Duracion)),
+cumpleRequisitos(Vocaloid, pequenio(Duracion)) :-
     vocaloid(Vocaloid, Cancion),
     cuantoDura(Cancion, Tiempo),
     Tiempo > Duracion.
@@ -93,4 +98,58 @@ puedeParticipar(Vocaloid, Concierto) :-
 
 
 
+elMasFamoso(Vocaloid) :-
+    nivelDeFama(Vocaloid, FamaMayor),
+    forall(nivelDeFama(_, OtroNivel) , FamaMayor >= OtroNivel).
 
+
+
+
+
+nivelDeFama(Vocaloid, Fama) :-
+    vocaloid(Vocaloid, _),
+    cuantasCanciones(Vocaloid, Canciones),
+    famaPorConciertos(Vocaloid, FamaConcierto),
+    Fama is FamaConcierto * Canciones.
+
+
+famaPorConciertos(Vocaloid, Fama) :-
+    findall(Nivel, puedeParticiparConNivel(Vocaloid, _, Nivel) , ListaFama),
+    sum_list(ListaFama, Fama).
+    
+
+
+
+puedeParticiparConNivel(Vocaloid, Concierto, Nivel) :-
+    puedeParticipar(Vocaloid, Concierto),
+    concierto(Concierto, _, Nivel, _).
+
+
+
+% 4
+
+conoceA(megurineLuka, hatsuneMiku).
+conoceA(megurineLuka, gumi).
+conoceA(gumi, seeU).
+conoceA(seeU, kaito).
+
+
+esConocido(Vocaloid, OtroVocaloid) :-
+    conoceA(Vocaloid, OtroVocaloid).
+
+esConocido(Vocaloid, OtroVocaloid) :-
+    conoceA(Vocaloid, OtroMasVocaloid),
+    conoceA(OtroMasVocaloid, OtroVocaloid).
+
+
+
+participaSolo(Vocaloid, Concierto) :-
+    vocaloid(Vocaloid, _),
+    concierto(Concierto, _, _, _),
+    forall(puedeParticipar(OtroVocaloid, Concierto) , not(esConocido(Vocaloid, OtroVocaloid))).
+
+
+
+% 5
+
+% teniendo los datos necesarios, agregaria un tipo mas de concierto, cambiando, por ejemplo. si puede participar en el concierto
