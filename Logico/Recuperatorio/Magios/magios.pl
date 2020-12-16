5b0dad7faba788a2d2d4afd93b76e6f7634e912f
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2020-12-16 17:27:39 -0300

    Termine magios

--- Logico/Recuperatorio/Magios/magios.pl
+++ Logico/Recuperatorio/Magios/magios.pl
@@ -24,18 +24,6 @@ salvo(carl,lenny).
 salvo(homero,larry).
 salvo(otto,burns).
 
-%Los beneficios son funtores confort(descripcion), confort(descripcion, caracteristica),
-% dispersion(descripcion), economico(descripcion, monto).
-
-
-gozaBeneficio(carl, confort(sillon)).
-gozaBeneficio(lenny, confort(sillon)).
-gozaBeneficio(lenny, confort(estacionamiento, techado)).
-gozaBeneficio(carl, confort(estacionamiento, libre)).
-gozaBeneficio(clark, confort(viajeSinTrafico)).
-gozaBeneficio(clark, dispersion(fiestas)).
-gozaBeneficio( burns , dispersion(fiestas)).
-gozaBeneficio(lenny, economico(descuento, 500)).
 
 
 
@@ -68,18 +56,19 @@ aspiranteMagio(Persona):-
 
 
 
+
 %%%%%%%%%%%%%
 %% Punto 2 %%
 %%%%%%%%%%%%%
 
 estaAlMando(UnMagio):-
-    persona(alMando(Persona,_)).
+    persona(alMando(UnMagio,_)).
 
 esNovato(UnMagio):-
-    persona(novato(Persona)).
+    persona(novato(UnMagio)).
 
 esElegido(UnMagio):-
-    persona(elElegido(Persona)).
+    persona(elElegido(UnMagio)).
     
 
 
@@ -102,7 +91,128 @@ mayorNivel(UnMagio, _):-
     esElegido(UnMagio).
 
 
+%%%%%%%%%%%%%
+%% Punto 3 %%
+%%%%%%%%%%%%%
+
     
+
+sienteEnvidia(Envidioso, ListaDePersonas):-
+    persona(Envidioso),
+    findall(Persona , (envidiaA(Envidioso, Persona), Envidioso \= Persona), ListaDePersonas).
+
+
+envidiaA(UnaPersona, OtraPersona):-
+    aspiranteMagio(UnaPersona),
+    esMagio(OtraPersona).
+
+envidiaA(UnaPersona, OtraPersona):-
+    not(aspiranteMagio(UnaPersona)),
+    not(esMagio(UnaPersona)),
+    aspiranteMagio(OtraPersona).
+
+envidiaA(UnaPersona, OtraPersona):-
+    esNovato(UnaPersona),
+    estaAlMando(OtraPersona).
+
+
+
+
+%%%%%%%%%%%%%
+%% Punto 4 %%
+%%%%%%%%%%%%%
+
+
+
+masEnvidioso(Persona):-
+    persona(Persona),
+    forall((persona(OtraPersona) , OtraPersona \= Persona) , envidiaMas(Persona, OtraPersona)).
+
+envidiaMas(UnaPersona, OtraPersona):-
+    sienteEnvidia(UnaPersona, UnaLista),
+    sienteEnvidia(OtraPersona, OtraLista),
+    UnaPersona \= OtraPersona,
+    length(UnaLista, UnaCantidad),
+    length(OtraLista, OtraCantidad),
+    UnaCantidad >= OtraCantidad.
     
-    
-    
+
+
+%%%%%%%%%%%%%
+%% Punto 5 %%
+%%%%%%%%%%%%%
+
+%Los beneficios son funtores confort(descripcion), confort(descripcion, caracteristica),
+% dispersion(descripcion), economico(descripcion, monto).
+
+
+gozaBeneficio(carl, confort(sillon)).
+gozaBeneficio(lenny, confort(sillon)).
+gozaBeneficio(lenny, confort(estacionamiento, techado)).
+gozaBeneficio(carl, confort(estacionamiento, libre)).
+gozaBeneficio(clark, confort(viajeSinTrafico)).
+gozaBeneficio(clark, dispersion(fiestas)).
+gozaBeneficio( burns , dispersion(fiestas)).
+gozaBeneficio(lenny, economico(descuento, 500)).
+
+
+soloLoGoza(Persona, Beneficio):-
+    gozaBeneficio(Persona, Beneficio),
+    not((gozaBeneficio(OtraPersona, Beneficio) , OtraPersona \= Persona)).
+
+
+
+
+%%%%%%%%%%%%%
+%% Punto 6 %%
+%%%%%%%%%%%%%
+
+
+% tipoDeBeneficioMasAprovechado(Beneficio):-
+%     gozaBeneficio(_ , Beneficio),
+%     forall((gozaBeneficio(_, OtroBeneficio), Beneficio \= OtroBeneficio) , loUsanMas(Beneficio, OtroBeneficio)).
+
+% cuantosLoUsan(Beneficio, Cantidad):-
+%     gozaBeneficio(_, Beneficio),
+%     findall(Persona, gozaBeneficio(Persona, Beneficio) , ListaDePersonas),
+%     length(ListaDePersonas, Cantidad).
+
+
+
+% loUsanMas(UnBeneficio, OtroBeneficio):-
+%     cuantosLoUsan(UnBeneficio, Cuantos),
+%     cuantosLoUsan(OtroBeneficio, OtrosCuantos),
+%     Cuantos >= OtrosCuantos.
+
+
+tipoDeBeneficioMasAprovechado(Tipo) :-
+    tipoDeBeneficio(_, Tipo),
+    forall(tipoDeBeneficio(_, OtroTipo) , supera(Tipo, OtroTipo)).
+
+
+
+supera(Tipo, OtroTipo) :-
+    tipoDeBeneficio(_, Tipo),
+    tipoDeBeneficio(_, OtroTipo),
+    cuantosLoUsan(Tipo, Cuantos),
+    cuantosLoUsan(OtroTipo, OtrosCuantos),
+    Cuantos >= OtrosCuantos.
+
+
+cuantosLoUsan(Tipo, Cuantos) :-
+    gozaBeneficio(_, Beneficio),
+    tipoDeBeneficio(Beneficio, Tipo),
+    findall(Magio, gozaBeneficioPorTipo(Magio, Tipo) , Magios),
+    length(Magios, Cuantos).
+
+
+
+tipoDeBeneficio(confort(_), confort).
+tipoDeBeneficio(confort(_, _), confort).
+tipoDeBeneficio(dispersion(_), dispersion).
+tipoDeBeneficio(economico(_,_), economico).
+
+
+gozaBeneficioPorTipo(Magio, Tipo) :-
+    gozaBeneficio(Magio, Beneficio),
+    tipoDeBeneficio(Beneficio, Tipo).
