a411be7f139a5ab496559746b6e2f85a13520671
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2020-12-17 11:53:14 -0300

    empece suenios

---
+++ Logico/Recuperatorio/Suenios/suenios.pl
@@ -0,0 +1,31 @@
+
+
+%%%%%%%%%%%%%
+%% Punto 1 %%
+%%%%%%%%%%%%%
+
+
+% creencias
+
+creeEn(gabriel, campanita).
+creeEn(gabriel, magoDeOz).
+creeEn(gabriel, cavenaghi).
+creeEn(juan, conejoDePascua).
+creeEn(macarena, reyesMagos).
+creeEn(macarena, magoCapria).
+creeEn(macarena, campanita).
+
+
+% tipos de suenios
+
+sueniaCon(gabriel, suenio(ganarLoteria , [5,9])).
+sueniaCon(gabriel, suenio(serFutbolista, arsenal)).
+sueniaCon(juan , suenio(serCantante, 100000)).
+sueniaCon(macarena, suenio(serCantante, 10000)).
+
+
+
+%%%%%%%%%%%%%
+%% Punto 1 %%
+%%%%%%%%%%%%%
+
